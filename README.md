6dd86ab7755ec8938d5176a7f33609ba94eacb39
AsvalGTA <valentinb44800@gmail.com>
2019-04-06 23:04:23 +0200

    Release v1.0

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # AESBot
+[![](https://img.shields.io/badge/License-GPL-blue.svg?logo=gnu)](https://github.com/iAmAsval/AESBot/blob/master/LICENSE)
+[![](https://img.shields.io/badge/Twitter-@AsvalFN-1da1f2.svg?logo=twitter)](https://twitter.com/AsvalFN)
+[![](https://img.shields.io/badge/Discord-Need%20Help%3F-7289da.svg?logo=discord)](https://discord.gg/JmWvXKb)
+
+**A Discord bot to post Fortnite AES Keys built in Javascript and made for Heroku**
+
+## GETTING STARTED
+### Prerequisites
+[Install Node.js](https://nodejs.org/en/)
+### How does it works
+**1.** Install all needed npm dependencies
+```js
+npm install axios
+npm install discord.js
+npm install express
+npm install fortnite-api
+npm install node-cron
+```
+**2.** Create your [Discord Bot](https://discordapp.com/developers/applications/)
+
+**3.** Create your [Heroku App](https://dashboard.heroku.com/apps)
+  - Go to `Settings`, `Add Buildpack` and select `NodeJS`
+  - Go to `Settings`, `Reveal Config Vars` and add:
+    - `aesCHANNELID` - the discord channel id where dynamic keys will be posted
+    - `botPREFIX`
+    - `botTOKEN`
+    - `epicEMAIL` - [Fortnite-API](https://www.npmjs.com/package/fortnite-api) access_token used to ask the dynamic AES Keys
+    - `epicPASSWORD` - [Fortnite-API](https://www.npmjs.com/package/fortnite-api) access_token used to ask the dynamic AES Keys
+    - `sAESKEY` - the message posted when `aes` command is used
+  - Go to `Deploy` and follow the steps depending on your deployment method
+  
+**4.** Your bot is now running 24/7 and is pinged each 5 minutes to keep it alive on heroku. Dynamic AES Keys are checked everyday at shop rotation time + 5 seconds, however if you wanna check if a key has been released early, use `faes`.
+    
+## DOCUMENTATION
+### Features
+1. `aes` - Post Fortnite Static AES Key
+2. `faes` - Post Fortnite Dynamic AES Keys (Moderator role needed)
+3. `clear amount` - Clear messages, default amount is set to 100 (User must have Manage Messages permission)
+4. `help` - Summarize the commands
+### What i'm using
+- [Axios](https://www.npmjs.com/package/axios)
+- [Discord.js](https://discord.js.org/#/)
+- [Express](https://www.npmjs.com/package/express)
+- [Fortnite-API](https://www.npmjs.com/package/fortnite-api)
+- [node-cron](https://www.npmjs.com/package/node-cron)
+
+## TODO
+- [ ] Better way to detect new keys
---
+++ src/AESBot.js
@@ -0,0 +1,228 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const Fortnite = require("fortnite-api");
+const axios = require("axios");
+
+//HEROKU PORT
+var express = require('express');
+var app     = express();
+app.set('port', (process.env.PORT || 5000));
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+//HEROKU NO IDLE
+var http = require("http");
+setInterval(function() {
+    http.get("http://fn-aesbot.herokuapp.com/");
+}, 300000);
+
+//FORTNITE LOGIN
+let fortniteAPI = new Fortnite(
+    [
+        process.env.epicEMAIL,
+        process.env.epicPASSWORD,
+        "MzQ0NmNkNzI2OTRjNGE0NDg1ZDgxYjc3YWRiYjIxNDE6OTIwOWQ0YTVlMjVhNDU3ZmI5YjA3NDg5ZDMxM2I0MWE=",
+        "ZWM2ODRiOGM2ODdmNDc5ZmFkZWEzY2IyYWQ4M2Y1YzY6ZTFmMzFjMjExZjI4NDEzMTg2MjYyZDM3YTEzZmM4NGQ="
+    ],
+    {
+        debug: true
+    }
+);
+
+//BOT LOGIN
+client.on('ready', () => {
+    console.log("Connected as " + client.user.tag)
+    console.log("Servers:")
+    client.guilds.forEach((guild) => {
+        console.log("   - " + guild.name)
+    })
+    client.user.setStatus("dnd");
+    client.user.setActivity("Fortnite AES Keys", { type: "WATCHING"});
+
+    var cron = require('node-cron');
+    fortniteAPI.login().then(() => {
+        cron.schedule('5 0 0 * * *', () => { // UTC from heroku --> shop rotation + 5 seconds
+            getAESKeys();
+          });
+    });
+});
+//BOT PREFIX
+client.on('message', (receivedMessage) => {
+    if (receivedMessage.author == client.user) { // Prevent bot from responding to its own messages
+        return
+    }
+    
+    if (receivedMessage.content.startsWith(process.env.botPREFIX)) {
+        processCommand(receivedMessage)
+    }
+});
+//BOT COMMANDS
+function processCommand(receivedMessage) {
+    let fullCommand = receivedMessage.content.substr(1) // Remove the leading exclamation mark
+    let splitCommand = fullCommand.split(" ") // Split the message up in to pieces for each space
+    let primaryCommand = splitCommand[0] // The first word directly after the exclamation is the command
+    let arguments = splitCommand.slice(1) // All other words are arguments/parameters/options for the command
+
+    console.log("Command: >" + primaryCommand)
+    console.log("Author: " + receivedMessage.author.tag + "\t\t" + receivedMessage.author)
+    console.log("Arguments: " + arguments) // There may not be any arguments
+    console.log("")
+
+    if (primaryCommand == "aes") {
+        receivedMessage.channel.send(receivedMessage.author + " " + process.env.sAESKEY)
+    }
+    else if (primaryCommand == "faes")
+    {
+        if (receivedMessage.member.roles.some(role => role.name === 'Moderator'))
+        {
+            receivedMessage.delete();
+            getAESKeys();
+        }
+        else
+        {
+            receivedMessage.channel.send(receivedMessage.author + " You don't have enough permissions to use this command.");
+        }
+    }
+    else if (primaryCommand == "clear")
+    {
+        if (receivedMessage.member.hasPermission("MANAGE_MESSAGES")) {
+            receivedMessage.channel.fetchMessages({ limit: parseInt(arguments) + 1 })
+               .then(function(list){
+                   receivedMessage.channel.bulkDelete(list);
+                }, function(err){console.log(err)})
+        }
+    }
+    else if (primaryCommand == "help")
+    {
+        help(receivedMessage);
+    }
+    else {
+        receivedMessage.channel.send("I don't understand the command. Try `" + process.env.botPREFIX + "aes` or `" + process.env.botPREFIX + "faes`")
+    }
+}
+//BOT HELP
+function help(receivedMessage)
+{
+    const embed = new Discord.RichEmbed()
+  .setAuthor(client.user.username + "'s Help", client.user.avatarURL)
+  .setColor([119, 57, 59])
+  //.setThumbnail(client.user.avatarURL)
+  //.setTimestamp()
+  .addField("Main Commands",
+  "`" + process.env.botPREFIX + "aes` Sends the latest Fortnite Static AES Key\n")
+  .addField("Mods Command", "`" + process.env.botPREFIX + "faes` Force to update the latest Fortnite Dynamic AES Keys in <#" + process.env.aesCHANNELID + ">\n`" + process.env.botPREFIX + "clear amount` It does what it says, default amount is set to 100\n")
+
+  receivedMessage.channel.send({embed});
+}
+
+//FORTNITE LATEST PAKS GUID
+var dict = {
+    "260115131-230188842-1133188118-2617540933": "pakchunk1000-WindowsClient.pak",
+    "505583406-4147402688-1225192698-466108321": "pakchunk1001-WindowsClient.pak",
+    "1435256579-1146701242-28877991-4121881694": "pakchunk1002-WindowsClient.pak",
+};
+function splitGUID(input, len) {
+    return input.match(new RegExp('.{1,' + len + '}(?=(.{' + len + '})+(?!.))|.{1,' + len + '}$', 'g'))
+};
+
+//FORTNITE GET AES
+var fs = require('fs');
+function getAESKeys() {
+    try {
+        doStuff();
+      }
+      catch(error) {
+        console.log(error);
+      }
+};
+function doStuff()
+{
+    let headers = {};
+    headers["X-EpicGames-Language"] = "en";
+    headers["Authorization"] = "bearer " + fortniteAPI.access_token;
+    axios({
+        url: "https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/storefront/v2/keychain",
+        method: "GET",
+        headers : headers,
+        responseType: "json"
+    }).then(data => {
+        var sKeys = data.data;
+        fs.writeFileSync("aes.txt", sKeys.toString().split(',').join('\n'), function(err) {
+            if(err) {
+                return console.log(err);
+            }
+        });
+
+        var lineReader = require('readline').createInterface({
+            input: require('fs').createReadStream('aes.txt')
+          });
+
+        if (fs.existsSync("AESKeys.txt")) {
+            var data = fs.readFileSync('AESKeys.txt', 'utf-8');
+            var newValue = data.replace(data, '');
+            fs.writeFileSync('AESKeys.txt', newValue, 'utf-8');
+            console.log("AESKeys.txt cleared");
+        }
+        else
+        {
+            fs.writeFileSync('AESKeys.txt', '', 'utf-8');
+            console.log("AESKeys.txt created");
+        }
+
+        lineReader.on('line', function (line) {
+            var parts = line.split(':');
+
+            var guid = splitGUID(parts[0], 8);
+            var pakGUID = "";
+            var c = 0;
+            for (var i = 0; i < guid.length; i++)
+            {
+                c += 1;
+                if (c != guid.length)
+                {
+                    pakGUID += parseInt(guid[i], 16) + "-";
+                }
+                else
+                {
+                    pakGUID += parseInt(guid[i], 16);
+                }
+            }
+
+            var key = parts[1];
+            var aeskey = "";
+            try{
+                let buff = Buffer.from(key, 'base64').toString('hex');
+                aeskey = buff;
+            }
+            catch(error) {
+                console.log(error);
+              }
+
+            var item = parts[2];
+            
+            var output = "";
+            var result = dict[pakGUID] === undefined;
+            if (!result)
+            {
+                output += "0x" + aeskey.toString().replace('-', '').toUpperCase() + " - **" + item + "** - " + dict[pakGUID] + "\n";
+                client.channels.get(process.env.aesCHANNELID).send(output);
+            }
+            else
+            {
+                output += "0x" + aeskey.toString().replace('-', '').toUpperCase() + " - " + item + "\n";
+            }
+            fs.appendFileSync('AESKeys.txt', output, function (err) {
+                if (err) console.log(err);
+            });
+        });
+        console.log("Keys added to file");
+    }).catch(err => {
+        console.log(err);
+    });
+};
+
+bot_secret_token = process.env.botTOKEN;
+client.login(bot_secret_token);
